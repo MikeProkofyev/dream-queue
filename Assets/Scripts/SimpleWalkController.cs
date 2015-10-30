539913e3541305990fcb3923f7782b8409f21bdb
Mike Prokofyev <mikeprokofyev@gmail.com>
2015-10-31 00:50:11 +0500

    lvl upd

---
+++ Assets/Scripts/SimpleWalkController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleWalkController : MonoBehaviour {
+
+
+	float speed = 5.0F;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float vertical_movement = Input.GetAxis("Vertical") * speed;
+		float horizontal_movement = Input.GetAxis("Horizontal") * speed;
+		transform.Translate(horizontal_movement, 0, vertical_movement);
+
+
+
+	}
+}
