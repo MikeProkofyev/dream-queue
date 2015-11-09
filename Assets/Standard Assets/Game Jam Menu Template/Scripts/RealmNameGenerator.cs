edf4a82e66356cd9ba5fbbee5416bbc8f66df5b2
Mike Prokofyev <mikeprokofyev@gmail.com>
2015-11-09 15:07:39 +0500

    update! new level entirely. Hope it's the last. Faster pussycat, kill! kill!

---
+++ Assets/Standard Assets/Game Jam Menu Template/Scripts/RealmNameGenerator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	List <string> realms = new List<string> {"Puffy clouds", "Laura"};
+
+
+	public string GetRealmName () {
+		return realms[Random.Range(0, realms.Count)];
+	}
+}
